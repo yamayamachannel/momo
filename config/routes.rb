0f6dc5005f4b22e980c0f94519c99b34e6493bcc
yamato <yamato.yuusuke669@mail.kyutech.jp>
2019-10-29 16:06:54 +0900

    tasu1000

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   resources :memos
+  get 'memos/:id/add', to: 'memos#add'
   root "home#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
