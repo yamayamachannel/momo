8350685a382d0dfafe280c0d27044be39b68fac0
yamato <yamato.yuusuke669@mail.kyutech.jp>
2019-10-29 16:29:26 +0900

    hiku

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -68,6 +68,13 @@ class MemosController < ApplicationController
     redirect_to :action => 'show'
   end
 
+  def maina
+    @memo = Memo.find(params[:id])
+    @memo.bank -= 1000
+    @memo.save
+    redirect_to :action => 'show'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_memo
