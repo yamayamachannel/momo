d805fa43ce07919c73f5afb71fd38212259be34e
MasatoshiOsugi <q111004m@mail.kyutech.jp>
2019-12-03 16:33:06 +0900

    メールアドレスの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         memos_path
     end
+
+    # protect_form_forgery with: :exception
+
+    before_action :authenticate_user!
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    end
 end
