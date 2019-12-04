7bc2be3532c2f1db46f7dad8a19f951906ae49b5
kazuya <mihara.kazuya514@mail.kyutech.jp>
2019-12-05 02:00:16 +0900

    ユーザー名登録

--- db/migrate/20191025170257_devise_create_users.rb
+++ db/migrate/20191025170257_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :username,            null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -35,8 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps null: false
     end
-
-    add_index :users, :email,                unique: true
+    add_index :users, :username,              unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_162145) do
-
-  create_table "memos", force: :cascade do |t|
-    t.string "title"
-    t.integer "bank"
-    t.integer "wallet"
-    t.integer "sum"
-    t.integer "plus"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_memos_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.integer "bank"
-    t.integer "wallet"
-    t.integer "sum"
-    t.integer "plus"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
