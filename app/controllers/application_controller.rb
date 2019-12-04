699779cc09c94be59635ebe9ae30d1136bb0e65b
kazuya <mihara.kazuya514@mail.kyutech.jp>
2019-12-05 01:29:18 +0900

    username追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-    
+    before_action :configure_permitted_parameters, if: :devise_controller?
     def after_sign_in_path_for(resource)
         memos_path
     end
-
-end
+    protected
+  
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+  end
