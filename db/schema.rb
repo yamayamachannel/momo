3978775096fa0005d117611fbdab32dd9606ed44
miyashita <miyashita@ist26.istlab.info>
2019-12-05 15:19:59 +0900

    メアド修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_05_083603) do
+
+  create_table "memos", force: :cascade do |t|
+    t.string "title"
+    t.integer "bank"
+    t.integer "wallet"
+    t.integer "sum"
+    t.integer "plus"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "bank"
+    t.integer "wallet"
+    t.integer "sum"
+    t.integer "plus"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "username", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 
 end
