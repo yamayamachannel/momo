67e5e9bcbd5cb14c69d8a4e6a4e6c6816eec26c7
kazuya <mihara.kazuya514@mail.kyutech.jp>
2020-01-08 02:59:20 +0900

    履歴の向きを変更

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -4,7 +4,7 @@ class LogsController < ApplicationController
         @user = current_user
         @memos = Memo.where(user: @user)
         @memo = params[:me]
-        @logs = Log.where(memo_id: @memo)
+        @logs = Log.where(memo_id: @memo).order(:money, :minus, :sum, :comment, :created_at)
     end
 
 end
